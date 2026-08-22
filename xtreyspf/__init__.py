0daf9e24371a0aa2e630bad9b399835e0c3b4601
TheCursedOne <bingchiliingsacker@gmail.com>
2026-08-22 16:47:48 +0800

    Add equity functions to module exports

--- xtreyspf/__init__.py
+++ xtreyspf/__init__.py
@@ -1,3 +1,8 @@
 from .evaluator import preflop_eval
+from .equity import approximate_equity, accurate_equity
 
-__all__ = ["preflop_eval"]
+__all__ = [
+  'preflop_eval',
+  'approximate_equity',
+  'accurate_equity'
+]
