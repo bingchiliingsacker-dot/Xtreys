84c00d51f865d5740efb8b352493408f8082403e
TheCursedOne <bingchiliingsacker@gmail.com>
2026-08-23 17:58:16 +0800

    Add relative_equity to __all__ exports

--- xtreys/__init__.py
+++ xtreys/__init__.py
@@ -1,8 +1,9 @@
 from .evaluator import preflop_eval
-from .equity import approximate_equity, accurate_equity
+from .equity import approximate_equity, accurate_equity, relative_equity
 
 __all__ = [
   'preflop_eval',
   'approximate_equity',
-  'accurate_equity'
+  'accurate_equity',
+  'relative_equity'
 ]
